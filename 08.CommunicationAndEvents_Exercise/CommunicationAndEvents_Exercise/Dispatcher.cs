d9aa1378c5f7cb9b8e6b784d6f40e086957cc989
Mayya Peneva <maya.peneva@gmail.com>
2024-07-19 20:30:25 +0300

    Add files via upload

---
+++ 08.CommunicationAndEvents_Exercise/CommunicationAndEvents_Exercise/Dispatcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public delegate void NameChangeEventHandler(object sender, EventArgs args);
+
+public class Dispatcher
+{
+    public string Name { get; }
+}
+
+public class Handler
+{
+}
